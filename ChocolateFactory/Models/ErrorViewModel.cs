508d1d7a0c113c9e875124776e448c82d5434a2c
Lakshmi Vishal Hayagrivan <84403688+Lakshmi-vishal@users.noreply.github.com>
2024-02-05 17:51:57 +0100

    Delete ChocolateFactory directory
    Cleanup

--- ChocolateFactory/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChocolateFactory.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
