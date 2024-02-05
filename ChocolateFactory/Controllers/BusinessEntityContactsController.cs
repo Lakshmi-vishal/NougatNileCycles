4ccf792eb7809b1beaa7093c8ece2c5b1d154dda
Lakshmi-vishal <lakshmisuresh033.gmail.com>
2024-02-05 16:36:35 +0100

    changed directory

---
+++ ChocolateFactory/Controllers/BusinessEntityContactsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ChocolateFactory.Models;
+
+namespace ChocolateFactory.Controllers
+{
+    public class BusinessEntityContactsController : Controller
+    {
+        private readonly AdventureWorks2022Context _context;
+
+        public BusinessEntityContactsController(AdventureWorks2022Context context)
+        {
+            _context = context;
+        }
+
+        // GET: BusinessEntityContacts
+        public async Task<IActionResult> Index()
+        {
+            var adventureWorks2022Context = _context.BusinessEntityContacts.Include(b => b.BusinessEntity).Include(b => b.ContactType).Include(b => b.Person);
+            return View(await adventureWorks2022Context.ToListAsync());
+        }
+
+        // GET: BusinessEntityContacts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessEntityContact = await _context.BusinessEntityContacts
+                .Include(b => b.BusinessEntity)
+                .Include(b => b.ContactType)
+                .Include(b => b.Person)
+                .FirstOrDefaultAsync(m => m.BusinessEntityId == id);
+            if (businessEntityContact == null)
+            {
+                return NotFound();
+            }
+
+            return View(businessEntityContact);
+        }
+
+        // GET: BusinessEntityContacts/Create
+        public IActionResult Create()
+        {
+            ViewData["BusinessEntityId"] = new SelectList(_context.BusinessEntities, "BusinessEntityId", "BusinessEntityId");
+            ViewData["ContactTypeId"] = new SelectList(_context.ContactTypes, "ContactTypeId", "ContactTypeId");
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId");
+            return View();
+        }
+
+        // POST: BusinessEntityContacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BusinessEntityId,PersonId,ContactTypeId,Rowguid,ModifiedDate")] BusinessEntityContact businessEntityContact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(businessEntityContact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BusinessEntityId"] = new SelectList(_context.BusinessEntities, "BusinessEntityId", "BusinessEntityId", businessEntityContact.BusinessEntityId);
+            ViewData["ContactTypeId"] = new SelectList(_context.ContactTypes, "ContactTypeId", "ContactTypeId", businessEntityContact.ContactTypeId);
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId", businessEntityContact.PersonId);
+            return View(businessEntityContact);
+        }
+
+        // GET: BusinessEntityContacts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessEntityContact = await _context.BusinessEntityContacts.FindAsync(id);
+            if (businessEntityContact == null)
+            {
+                return NotFound();
+            }
+            ViewData["BusinessEntityId"] = new SelectList(_context.BusinessEntities, "BusinessEntityId", "BusinessEntityId", businessEntityContact.BusinessEntityId);
+            ViewData["ContactTypeId"] = new SelectList(_context.ContactTypes, "ContactTypeId", "ContactTypeId", businessEntityContact.ContactTypeId);
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId", businessEntityContact.PersonId);
+            return View(businessEntityContact);
+        }
+
+        // POST: BusinessEntityContacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BusinessEntityId,PersonId,ContactTypeId,Rowguid,ModifiedDate")] BusinessEntityContact businessEntityContact)
+        {
+            if (id != businessEntityContact.BusinessEntityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(businessEntityContact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BusinessEntityContactExists(businessEntityContact.BusinessEntityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BusinessEntityId"] = new SelectList(_context.BusinessEntities, "BusinessEntityId", "BusinessEntityId", businessEntityContact.BusinessEntityId);
+            ViewData["ContactTypeId"] = new SelectList(_context.ContactTypes, "ContactTypeId", "ContactTypeId", businessEntityContact.ContactTypeId);
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId", businessEntityContact.PersonId);
+            return View(businessEntityContact);
+        }
+
+        // GET: BusinessEntityContacts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessEntityContact = await _context.BusinessEntityContacts
+                .Include(b => b.BusinessEntity)
+                .Include(b => b.ContactType)
+                .Include(b => b.Person)
+                .FirstOrDefaultAsync(m => m.BusinessEntityId == id);
+            if (businessEntityContact == null)
+            {
+                return NotFound();
+            }
+
+            return View(businessEntityContact);
+        }
+
+        // POST: BusinessEntityContacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var businessEntityContact = await _context.BusinessEntityContacts.FindAsync(id);
+            if (businessEntityContact != null)
+            {
+                _context.BusinessEntityContacts.Remove(businessEntityContact);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BusinessEntityContactExists(int id)
+        {
+            return _context.BusinessEntityContacts.Any(e => e.BusinessEntityId == id);
+        }
+    }
+}
---
+++ ChocolateFactory/Controllers/CustomersController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ChocolateFactory.Models;
+
+namespace ChocolateFactory.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly AdventureWorks2022Context _context;
+
+        public CustomersController(AdventureWorks2022Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            var adventureWorks2022Context = _context.Customers.Include(c => c.Person).Include(c => c.Store).Include(c => c.Territory);
+            return View(await adventureWorks2022Context.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Person)
+                .Include(c => c.Store)
+                .Include(c => c.Territory)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId");
+            ViewData["StoreId"] = new SelectList(_context.Stores, "BusinessEntityId", "BusinessEntityId");
+            ViewData["TerritoryId"] = new SelectList(_context.SalesTerritories, "TerritoryId", "TerritoryId");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,PersonId,StoreId,TerritoryId,AccountNumber,Rowguid,ModifiedDate")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId", customer.PersonId);
+            ViewData["StoreId"] = new SelectList(_context.Stores, "BusinessEntityId", "BusinessEntityId", customer.StoreId);
+            ViewData["TerritoryId"] = new SelectList(_context.SalesTerritories, "TerritoryId", "TerritoryId", customer.TerritoryId);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId", customer.PersonId);
+            ViewData["StoreId"] = new SelectList(_context.Stores, "BusinessEntityId", "BusinessEntityId", customer.StoreId);
+            ViewData["TerritoryId"] = new SelectList(_context.SalesTerritories, "TerritoryId", "TerritoryId", customer.TerritoryId);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,PersonId,StoreId,TerritoryId,AccountNumber,Rowguid,ModifiedDate")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonId"] = new SelectList(_context.People, "BusinessEntityId", "BusinessEntityId", customer.PersonId);
+            ViewData["StoreId"] = new SelectList(_context.Stores, "BusinessEntityId", "BusinessEntityId", customer.StoreId);
+            ViewData["TerritoryId"] = new SelectList(_context.SalesTerritories, "TerritoryId", "TerritoryId", customer.TerritoryId);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Person)
+                .Include(c => c.Store)
+                .Include(c => c.Territory)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
